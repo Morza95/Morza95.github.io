7cf243c0f921b3bdbf3a896bb993c581ddf5da34
Morza95 <Buschendorf95@gmx.de>
2021-06-07 10:55:21 +0200

    Update
    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './components/Logo.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return ( 
+    <div className = "App" >
+        <header className = "App-header" >
+        <Logo></Logo>                                           {/*Einbindung des Logos Nav2Gö*/}
+        <p> Willkommen! </p>   
+        <p> Das ist eine App für ein Projektstudium. </p>
+        </header>         
+        <p className="App-beschreibung"> Hier folgt ein Text für die Beschreibung der App</p>
+
     </div>
-  );
+
+    );
 }
 
 export default App;
