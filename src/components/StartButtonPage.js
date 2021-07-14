bcba737bced08918ef76285e7956ed4874ca8acd
Kaiuby <kai.hoering@hotmail.de>
2021-07-14 17:20:20 +0200

    Texte hinzugefügt
    - Einleitung mit Datenschutzhinweisen
    - Use Case

--- src/components/StartButtonPage.js
+++ src/components/StartButtonPage.js
@@ -10,14 +10,37 @@ const StartButtonPage = () => {
       {clicked ? (
         <Startseite />
       ) : (
-        <button
-          class="buttonStartButton"
-          disabled={clicked}
-          onClick={() => setClicked(true)}
-        >
-          {" "}
-          Start Nav2Gö{" "}
-        </button>
+        <div>
+          <p id="UseCaseDescribtion">
+            Sehr geehrte(r) Teilnehmer(in),<br></br> im Rahmen unserer
+            Forschungsarbeit an der Georg-August-Universität Göttingen
+            beschäftigen wir uns mit dem Thema "Irgendwas mit Funk" und bitten
+            Sie zwei Web-Apps zu testen und an einer Befragung teilzunehmen. Die
+            Teilnahme dauert insgesamt ca. 5-10 Minuten.<br></br> Alle Angaben
+            werden selbstverständlich anonymisiert und vertraulich behandelt.
+            Die Daten werden ausschließlich zu wissenschaftlichen
+            Forschungszwecken verarbeitet und gespeichert. Eine Weitergabe an
+            Dritte ist ausgeschlossen. <br></br>Bitte lesen Sie die Beschreibung
+            in den Apps und die Fragen des Fragebogens sorgfältig durch und
+            beantworten Sie diese möglichst spontan, damit eine höhere
+            Aussagekraft gewährleistet ist. Es gibt keine richtigen oder
+            falschen Antworten. Kreuzen Sie deshalb bitte jeweils die Antwort
+            an, die am ehesten Ihrer Meinung entspricht.<br></br> Vielen Dank
+            für die Teilnahme! <br></br>Bei Fragen und Anmerkungen zu dieser
+            Studie wenden Sie sich gerne an:
+            <br></br>franz.buschendorf@stud.uni-goettingen.de
+            <br></br>katharina.girndt@stud.uni-goetinngen.de
+            <br></br>kai.hoering@stud.uni-goettingen.de{" "}
+          </p>
+          <button
+            class="buttonStartButton"
+            disabled={clicked}
+            onClick={() => setClicked(true)}
+          >
+            {" "}
+            Start Nav2Gö{" "}
+          </button>
+        </div>
       )}
     </div>
   );
