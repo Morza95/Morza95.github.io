bcea9859769ea6b8a8c0c4db977235dbbbbac20d
Morza95 <Buschendorf95@gmx.de>
2021-07-20 10:29:50 +0200

    Update 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+
+    <div>Hallo Welt</div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
